40d2c76a9dd06b550c0dfde7528d3b57c534597c
Parker Smith <parkerbsmith@gmail.com>
2015-09-21 14:38:47 +0000

    Moved logic out of ko computed method

--- app.js
+++ app.js
@@ -6,22 +6,7 @@ function AppViewModel() {
     this.roundDirection = ko.observable("up");
 
     this.tipAmount = ko.computed(function () {
-        var tipAmount = self.billAmount() * (self.tipPercent() / 100);
-        var total = +tipAmount + +self.billAmount();
-        console.debug("round: " + this.roundDirection());
-
-        var newTip;
-        if (self.roundDirection() === "up") {
-            var newTotal = Math.ceil(total);
-            var diff = newTotal - total;
-            newTip = tipAmount + diff;
-        } else {
-            var newTotal = Math.floor(total);
-            var diff = total - newTotal;
-            newTip = tipAmount - diff;
-        }
-
-        return newTip.toFixed(2);
+        return refreshValues(self.billAmount(), self.tipPercent(), self.roundDirection());
     }, this);
 
     this.roundedTotal = ko.computed(function () {
@@ -35,6 +20,38 @@ function AppViewModel() {
     this.tipToText = ko.computed(function () {
         return self.tipAmount() + " (" + self.actualTipPercent() + "%)";
     });
+    
+    this.refresh = function () {
+        return refreshValues(self.billAmount(), self.tipPercent(), self.roundDirection());
+    };
+    
+    function refreshValues(billAmount, tipPercent, roundDirection) {
+        if (isEmptyOrWhitespace(billAmount)) return;
+        if (isEmptyOrWhitespace(tipPercent)) return;
+        if (isEmptyOrWhitespace(roundDirection)) return;
+        if (roundDirection != "up" && round != "down") return;
+        
+        var tipAmount = billAmount * (tipPercent / 100);
+        var total = +tipAmount + +billAmount;
+        console.debug("round: " + roundDirection);
+
+        var newTip;
+        if (roundDirection === "up") {
+            var newTotal = Math.ceil(total);
+            var diff = newTotal - total;
+            newTip = tipAmount + diff;
+        } else {
+            var newTotal = Math.floor(total);var diff = total - newTotal;
+            newTip = tipAmount - diff;
+        }
+
+        return newTip.toFixed(2);
+    }
+    
+    function isEmptyOrWhitespace(str){
+        var result =  str.toString().trim() === '';
+        return result;
+    }
 }
 
 ko.applyBindings(new AppViewModel());
