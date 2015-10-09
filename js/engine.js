4e984c9d90626000e1eac7c8f7ec09441234617b
Parker Smith <parkerbsmith@gmail.com>
2015-10-08 21:33:36 -0400

    Input validation, output scrubbing

--- js/engine.js
+++ js/engine.js
@@ -9,7 +9,10 @@ Engine.calculateTip = function(billAmount, tipPercent, roundDirection) {
         if (Engine.isEmptyOrWhitespace(tipPercent)) return false;
         if (Engine.isEmptyOrWhitespace(roundDirection)) return false;
         if (roundDirection != "up" && roundDirection != "down") return false;
-        
+
+        if (tipPercent === "0") return 0;
+        if (billAmount < 0) billAmount = 0;
+
         var tipAmount = billAmount * (tipPercent / 100);
         var total = +tipAmount + +billAmount;
         console.debug("round: " + roundDirection);
@@ -25,5 +28,6 @@ Engine.calculateTip = function(billAmount, tipPercent, roundDirection) {
             newTip = tipAmount - diff;
         }
 
+        if (newTip < 0) newTip = 0;
         return newTip.toFixed(2);
 }
