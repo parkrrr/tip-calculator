dfe81d813ba1cd77e3519090e818fcfe281f86a5
Parker Smith <parkrrr@users.noreply.github.com>
2016-09-14 14:52:57 -0400

    Publish changes (#5)
    * Moved radio buttons to make more sense
    * Tests now run automatically

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+		'http-server': {
+			'test': {
+				port: 8080,
+				runInBackground: true
+			}
+		},
+        qunit: {
+			all: {
+				options: {
+					urls: ['http://localhost:8080/tests/index.html']
+				}
+			}
+		}
+	});
+
+    // Load plugin
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-http-server');
+
+	grunt.registerTask('default', []);
+    grunt.registerTask('test', ['http-server:test', 'qunit']);
+};
